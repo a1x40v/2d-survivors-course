9fbfdde87abe0767df62834e931a7d74ede5b9a8
a1x40v <a1x40v@gmail.com>
2023-09-04 19:24:53 +0300

    ExperienceBar

--- scenes/manager/ExpirienceManager.cs
+++ scenes/manager/ExpirienceManager.cs
@@ -2,7 +2,14 @@ using Godot;
 
 public partial class ExpirienceManager : Node
 {
+	public const float TargetExperienceGrow = 5;
+
+	[Signal]
+	public delegate void ExpirienceUpdatedEventHandler(float currentExp, float targetExp);
+
 	private float _currentExp;
+	private float _targetExperience = 5;
+	private int _currentLevel = 1;
 
 	public override void _Ready()
 	{
@@ -12,7 +19,16 @@ public partial class ExpirienceManager : Node
 
 	public void IncrementExpirience(float amount)
 	{
-		_currentExp += amount;
+		_currentExp = Mathf.Min(_currentExp + amount, _targetExperience);
+		EmitSignal("ExpirienceUpdated", _currentExp, _targetExperience);
+
+		if (_currentExp == _targetExperience)
+		{
+			_currentLevel++;
+			_targetExperience += TargetExperienceGrow;
+			_currentExp = 0;
+			EmitSignal("ExpirienceUpdated", _currentExp, _targetExperience);
+		}
 		GD.Print(_currentExp);
 	}
 
