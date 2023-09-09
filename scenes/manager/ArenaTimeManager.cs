0426ebf1eab12aa449222b848a7cdb21543b72c6
a1x40v <a1x40v@gmail.com>
2023-09-09 13:55:03 +0300

    Increase difficulty over time

--- scenes/manager/ArenaTimeManager.cs
+++ scenes/manager/ArenaTimeManager.cs
@@ -2,15 +2,32 @@ using Godot;
 
 public partial class ArenaTimeManager : Node
 {
+	public const double DifficultyInterval = 5;
+
+	[Signal]
+	public delegate void ArenaDifficultyIncreasedEventHandler(int difficulty);
+
 	[Export]
 	public PackedScene EndScreenScene { get; set; }
 
+	private int _arenaDifficulty;
 	private Timer _timer;
 
 	public override void _Ready()
 	{
 		_timer = GetNode<Timer>("Timer");
 		_timer.Timeout += OnTimerTimeout;
+	}
+
+	public override void _Process(double delta)
+	{
+		var nextTimeTarget = _timer.WaitTime - (_arenaDifficulty + 1) * DifficultyInterval;
+
+		if (_timer.TimeLeft <= nextTimeTarget)
+		{
+			_arenaDifficulty += 1;
+			EmitSignal("ArenaDifficultyIncreased", _arenaDifficulty);
+		}
 	}
 
 	public double GetTimeElapsed()
