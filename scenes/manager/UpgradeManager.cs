5e2d679609d6ed41694f4a5763145f03763c8a51
a1x40v <a1x40v@gmail.com>
2023-09-12 16:26:29 +0300

    Prevent Abilities from Being Chosen Twice

--- scenes/manager/UpgradeManager.cs
+++ scenes/manager/UpgradeManager.cs
@@ -4,70 +4,80 @@ using Godot.Collections;
 
 public partial class UpgradeManager : Node
 {
-	[Export]
-	public Array<AbilityUpgrade> UpgradePool { get; set; }
+    [Export]
+    public Array<AbilityUpgrade> UpgradePool { get; set; }
 
-	[Export]
-	public Node ExperienceManager { get; set; }
+    [Export]
+    public Node ExperienceManager { get; set; }
 
-	[Export]
-	public PackedScene UpgradeScreenScene { get; set; }
+    [Export]
+    public PackedScene UpgradeScreenScene { get; set; }
 
-	public Dictionary<string, Dictionary> CurrentUpgrades { get; set; } = new Dictionary<string, Dictionary>();
+    public Dictionary<string, Dictionary> CurrentUpgrades { get; set; } = new Dictionary<string, Dictionary>();
 
-	public override void _Ready()
-	{
-		ExperienceManager.Connect("LevelUp", new Callable(this, nameof(OnLevelUp)));
-	}
+    public override void _Ready()
+    {
+        ExperienceManager.Connect("LevelUp", new Callable(this, nameof(OnLevelUp)));
+    }
 
-	private Array<AbilityUpgrade> pickUpgrades()
-	{
-		var chosenUpgrades = new Array<AbilityUpgrade> { };
-		var filteredUpgrades = UpgradePool.Duplicate();
-		for (int i = 0; i < 2; i++)
-		{
-			var chosenUpgrade = filteredUpgrades.PickRandom();
-			chosenUpgrades.Add(chosenUpgrade);
-			filteredUpgrades = new Array<AbilityUpgrade>(filteredUpgrades.Where(x => x.Id != chosenUpgrade.Id));
-		}
+    private Array<AbilityUpgrade> pickUpgrades()
+    {
+        var chosenUpgrades = new Array<AbilityUpgrade> { };
+        var filteredUpgrades = UpgradePool.Duplicate();
+        for (int i = 0; i < 2; i++)
+        {
+            if (filteredUpgrades.Count == 0) break;
+            var chosenUpgrade = filteredUpgrades.PickRandom();
+            chosenUpgrades.Add(chosenUpgrade);
+            filteredUpgrades = new Array<AbilityUpgrade>(filteredUpgrades.Where(x => x.Id != chosenUpgrade.Id));
+        }
 
-		return chosenUpgrades;
-	}
+        return chosenUpgrades;
+    }
 
-	public void ApplyUpgrade(AbilityUpgrade upgrade)
-	{
-		var hasUpgrade = CurrentUpgrades.ContainsKey(upgrade.Id);
-		if (!hasUpgrade)
-		{
-			var dic = new Dictionary
-			{
-				{ "resource", upgrade },
-				{ "quantity", 1 }
-			};
-			CurrentUpgrades.Add(upgrade.Id, dic);
-		}
-		else
-		{
-			CurrentUpgrades[upgrade.Id]["quantity"] = (int)CurrentUpgrades[upgrade.Id]["quantity"] + 1;
-		}
+    public void ApplyUpgrade(AbilityUpgrade upgrade)
+    {
+        var hasUpgrade = CurrentUpgrades.ContainsKey(upgrade.Id);
+        if (!hasUpgrade)
+        {
+            var dic = new Dictionary
+            {
+                { "resource", upgrade },
+                { "quantity", 1 }
+            };
+            CurrentUpgrades.Add(upgrade.Id, dic);
+        }
+        else
+        {
+            CurrentUpgrades[upgrade.Id]["quantity"] = (int)CurrentUpgrades[upgrade.Id]["quantity"] + 1;
+        }
 
-		var gameEvents = GetNode<GameEvents>("/root/GameEvents");
-		gameEvents.EmitAbilityUpgradeAdded(upgrade, CurrentUpgrades);
+        if (upgrade.MaxQuantity > 0)
+        {
+            var currentQuantity = (int)CurrentUpgrades[upgrade.Id]["quantity"];
+            if (currentQuantity == upgrade.MaxQuantity)
+            {
+                UpgradePool = new Array<AbilityUpgrade>(UpgradePool.Where(x => x.Id != upgrade.Id));
+            }
+        }
 
-		GD.Print(CurrentUpgrades);
-	}
+        var gameEvents = GetNode<GameEvents>("/root/GameEvents");
+        gameEvents.EmitAbilityUpgradeAdded(upgrade, CurrentUpgrades);
 
-	public void OnLevelUp(int currentLevel)
-	{
-		var upgradeScreenInstance = UpgradeScreenScene.Instantiate() as UpgradeScreen;
-		AddChild(upgradeScreenInstance);
-		var chosenUpgrades = pickUpgrades();
-		upgradeScreenInstance.SetAbilityUpgrades(chosenUpgrades);
-		upgradeScreenInstance.Connect("UpgradeSelected", new Callable(this, nameof(OnUpgradeSelected)));
-	}
+        GD.Print(CurrentUpgrades);
+    }
 
-	public void OnUpgradeSelected(AbilityUpgrade upgrade)
-	{
-		ApplyUpgrade(upgrade);
-	}
+    public void OnLevelUp(int currentLevel)
+    {
+        var upgradeScreenInstance = UpgradeScreenScene.Instantiate() as UpgradeScreen;
+        AddChild(upgradeScreenInstance);
+        var chosenUpgrades = pickUpgrades();
+        upgradeScreenInstance.SetAbilityUpgrades(chosenUpgrades);
+        upgradeScreenInstance.Connect("UpgradeSelected", new Callable(this, nameof(OnUpgradeSelected)));
+    }
+
+    public void OnUpgradeSelected(AbilityUpgrade upgrade)
+    {
+        ApplyUpgrade(upgrade);
+    }
 }
